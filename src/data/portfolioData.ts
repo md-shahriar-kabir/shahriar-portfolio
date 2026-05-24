7c611f98b746a8ea8f61db1d35681f7e4dcc851d
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-05-25 00:59:10 +0600

    Update Resume Section

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -57,7 +57,7 @@ export const portfolioData = {
       facebook: "https://www.facebook.com/shariarkabir88",
       // twitter: "https://twitter.com/shahriar_kabir"
     },
-    resumeUrl: "#", // Anchor to trigger download or view resume
+    resumeUrl: "/public/shahriar-kabir-resume.pdf", // Anchor to trigger download or view resume
   },
   
   about: {
